cf3960dc69f82e7de6a3b4171066479775607f93
Alessandro Stranieri <alessandro.stranieri@gmail.com>
2019-06-22 22:55:48 +0300

    [Refactoring] Using loss from backend

--- sem_seg/models/losses.py
+++ sem_seg/models/losses.py
@@ -1,5 +1,5 @@
-from tensorflow.python.keras.losses import categorical_crossentropy
+from tensorflow.python.keras import backend as K
 
 
 def categorical_crossentropy_with_logits(y_true, y_pred):
-    return categorical_crossentropy(y_true=y_true, y_pred=y_pred, from_logits=True)
+    return K.categorical_crossentropy(y_true, y_pred, from_logits=True)
